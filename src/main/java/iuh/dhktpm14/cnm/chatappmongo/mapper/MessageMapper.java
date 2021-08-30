c0373031d081c5a818c9f41de30159133eac322b
maikiencuong <maikiencuongiuh@gmail.com>
2021-08-31 06:47:17 +0700

    [dev] MessageMapper: thêm hàm map từ message qua MessageToClient dto class

--- src/main/java/iuh/dhktpm14/cnm/chatappmongo/mapper/MessageMapper.java
+++ src/main/java/iuh/dhktpm14/cnm/chatappmongo/mapper/MessageMapper.java
@@ -1,6 +1,7 @@
 package iuh.dhktpm14.cnm.chatappmongo.mapper;
 
 import iuh.dhktpm14.cnm.chatappmongo.dto.MessageDto;
+import iuh.dhktpm14.cnm.chatappmongo.dto.MessageToClient;
 import iuh.dhktpm14.cnm.chatappmongo.entity.Message;
 import iuh.dhktpm14.cnm.chatappmongo.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,7 +33,10 @@ public class MessageMapper {
             return null;
         var dto = new MessageDto();
         dto.setId(message.getId());
-        dto.setSender(userMapper.toUserProfileDto(message.getSenderId()));
+        if (message.getSenderId() != null)
+            dto.setSender(userMapper.toUserProfileDto(message.getSenderId()));
+        else
+            dto.setSender(null);
         dto.setCreateAt(message.getCreateAt());
         dto.setType(message.getType());
         dto.setContent(message.getContent());
@@ -40,6 +44,26 @@ public class MessageMapper {
         dto.setDeleted(message.getDeleted());
         dto.setStatus(message.getStatus());
         dto.setReactions(message.getReactions());
+        return dto;
+    }
+
+    public MessageToClient toMessageToClient(Message message) {
+        if (message == null)
+            return null;
+        var dto = new MessageToClient();
+        dto.setId(message.getId());
+        if (message.getSenderId() != null)
+            dto.setSender(userMapper.toUserProfileDto(message.getSenderId()));
+        else
+            dto.setSender(null);
+        dto.setCreateAt(message.getCreateAt());
+        dto.setType(message.getType());
+        dto.setContent(message.getContent());
+        dto.setPin(message.getPin());
+        dto.setDeleted(message.getDeleted());
+        dto.setStatus(message.getStatus());
+        dto.setReactions(message.getReactions());
+        dto.setRoomId(message.getRoomId());
         return dto;
     }
 }
