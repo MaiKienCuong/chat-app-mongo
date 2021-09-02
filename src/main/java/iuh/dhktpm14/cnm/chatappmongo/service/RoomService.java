fdbc690f9bd7e139786d353482efeacb1cda8c3a
maikiencuong <maikiencuongiuh@gmail.com>
2021-09-03 06:38:05 +0700

    [dev] RoomService: thêm hàm thêm một list members vào room

--- src/main/java/iuh/dhktpm14/cnm/chatappmongo/service/RoomService.java
+++ src/main/java/iuh/dhktpm14/cnm/chatappmongo/service/RoomService.java
@@ -1,13 +1,37 @@
 package iuh.dhktpm14.cnm.chatappmongo.service;
 
-import iuh.dhktpm14.cnm.chatappmongo.repository.RoomRepository;
+import iuh.dhktpm14.cnm.chatappmongo.entity.Member;
+import iuh.dhktpm14.cnm.chatappmongo.entity.Room;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.BulkOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RoomService {
 
     @Autowired
-    private RoomRepository roomRepository;
+    private MongoTemplate mongoTemplate;
+
+    public void addMembersToRoom(List<Member> members, String toRoomId) {
+        BulkOperations ops = mongoTemplate.bulkOps(BulkOperations.BulkMode.UNORDERED, Room.class);
+        var i = 0;
+        for (Member member : members) {
+            var criteria = Criteria.where("_id").is(toRoomId);
+            var update = new Update();
+            update.push("members", member);
+            ops.updateOne(Query.query(criteria), update);
+            i++;
+            if (i % 20 == 0)
+                ops.execute();
+        }
+        if (i != 0)
+            ops.execute();
+    }
 
 }
