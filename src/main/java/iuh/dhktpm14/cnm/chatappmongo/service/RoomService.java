777886f15ced8a93370a7b1f6c040fa99d7c0f08
maikiencuong <maikiencuongiuh@gmail.com>
2021-09-03 13:29:19 +0700

    [dev] RoomService: thêm hàm lấy member theo userId, hàm kiểm tra xem có phải admin hay không - thêm hàm xóa một thành viên khỏi room

--- src/main/java/iuh/dhktpm14/cnm/chatappmongo/service/RoomService.java
+++ src/main/java/iuh/dhktpm14/cnm/chatappmongo/service/RoomService.java
@@ -2,6 +2,7 @@ package iuh.dhktpm14.cnm.chatappmongo.service;
 
 import iuh.dhktpm14.cnm.chatappmongo.entity.Member;
 import iuh.dhktpm14.cnm.chatappmongo.entity.Room;
+import iuh.dhktpm14.cnm.chatappmongo.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.BulkOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -11,6 +12,8 @@ import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class RoomService {
@@ -18,6 +21,12 @@ public class RoomService {
     @Autowired
     private MongoTemplate mongoTemplate;
 
+    @Autowired
+    private RoomRepository roomRepository;
+
+    /**
+     * thêm một list member vào room
+     */
     public void addMembersToRoom(List<Member> members, String toRoomId) {
         BulkOperations ops = mongoTemplate.bulkOps(BulkOperations.BulkMode.UNORDERED, Room.class);
         var i = 0;
@@ -32,6 +41,66 @@ public class RoomService {
         }
         if (i != 0)
             ops.execute();
+    }
+
+    /**
+     * lấy thành viên trong room theo id của member
+     */
+    public Optional<Member> getMemberFromRoomById(String userId, String roomId) {
+        Optional<Room> roomOptional = roomRepository.findById(roomId);
+        if (roomOptional.isPresent()) {
+            var room = roomOptional.get();
+            Set<Member> members = room.getMembers();
+            if (members != null && ! members.isEmpty()) {
+                return members.stream().filter(x -> x.getUserId().equals(userId)).findFirst();
+            }
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * kiểm tra xem userId này có phải admin của room hay không
+     */
+    public boolean isAdminOfRoom(String userId, String roomId) {
+        Optional<Member> optionalMember = getMemberFromRoomById(userId, roomId);
+        if (optionalMember.isPresent() && optionalMember.get().getIsAdmin() != null)
+            return optionalMember.get().getIsAdmin();
+        return false;
+    }
+
+    public boolean deleteMember(String deleteId, String roomId, String currentUserId) {
+        if (deleteId.equals(currentUserId))
+            return false;
+        if (canDelete(deleteId, roomId, currentUserId)) {
+            delete(deleteId, roomId);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * xóa trong database
+     */
+    private void delete(String deleteId, String roomId) {
+        var criteria = Criteria.where("_id").is(roomId);
+        var update = new Update();
+        update.pull("members", Query.query(Criteria.where("userId").is(deleteId)));
+        mongoTemplate.updateFirst(Query.query(criteria), update, Room.class);
+    }
+
+    private boolean canDelete(String deleteId, String roomId, String currentUserId) {
+        Optional<Member> optionalMember = getMemberFromRoomById(deleteId, roomId);
+        if (optionalMember.isPresent()) {
+            if (roomRepository.isCreatorRoom(currentUserId, roomId))
+                return true;
+            if (isAdminOfRoom(currentUserId, roomId)) {
+                if (isAdminOfRoom(deleteId, roomId))
+                    return false;
+                return true;
+            }
+            return currentUserId.equals(optionalMember.get().getAddByUserId());
+        }
+        return false;
     }
 
 }
