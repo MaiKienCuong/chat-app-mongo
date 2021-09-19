a0dd3fee9e417cca35209ea21bff06697aa91748
maikiencuong <maikiencuongiuh@gmail.com>
2021-09-19 15:38:57 +0700

    [dev] thêm api lấy inbox với một người bất kì - nếu chưa có room chung, thì nó trả về room với id là null, các thuộc tính khác đầy đủ - nếu id của room trả về null, trước khi gửi tin nhắn thì client gọi api tạo room trước, sau đó lấy id của room vừa tạo để gửi tin nhắn

--- src/main/java/iuh/dhktpm14/cnm/chatappmongo/rest/InboxRest.java
+++ src/main/java/iuh/dhktpm14/cnm/chatappmongo/rest/InboxRest.java
@@ -2,13 +2,21 @@ package iuh.dhktpm14.cnm.chatappmongo.rest;
 
 import io.swagger.annotations.ApiOperation;
 import iuh.dhktpm14.cnm.chatappmongo.dto.InboxDto;
+import iuh.dhktpm14.cnm.chatappmongo.dto.InboxSummaryDto;
+import iuh.dhktpm14.cnm.chatappmongo.dto.RoomGroupSummaryDto;
+import iuh.dhktpm14.cnm.chatappmongo.dto.RoomOneSummaryDto;
 import iuh.dhktpm14.cnm.chatappmongo.entity.Inbox;
+import iuh.dhktpm14.cnm.chatappmongo.entity.Member;
+import iuh.dhktpm14.cnm.chatappmongo.entity.Room;
 import iuh.dhktpm14.cnm.chatappmongo.entity.User;
+import iuh.dhktpm14.cnm.chatappmongo.enumvalue.RoomType;
 import iuh.dhktpm14.cnm.chatappmongo.exceptions.UnAuthenticateException;
 import iuh.dhktpm14.cnm.chatappmongo.mapper.InboxMapper;
+import iuh.dhktpm14.cnm.chatappmongo.mapper.UserMapper;
 import iuh.dhktpm14.cnm.chatappmongo.payload.MessageResponse;
 import iuh.dhktpm14.cnm.chatappmongo.repository.InboxMessageRepository;
 import iuh.dhktpm14.cnm.chatappmongo.repository.InboxRepository;
+import iuh.dhktpm14.cnm.chatappmongo.repository.RoomRepository;
 import iuh.dhktpm14.cnm.chatappmongo.service.InboxService;
 import iuh.dhktpm14.cnm.chatappmongo.service.ReadTrackingService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,12 +30,16 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
@@ -50,17 +62,26 @@ public class InboxRest {
     @Autowired
     private ReadTrackingService readTrackingService;
 
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Autowired
+    private UserMapper userMapper;
+
     /**
      * lấy tất cả inbox của người dùng hiện tại
      */
     @GetMapping
     @PreAuthorize("isAuthenticated()")
     @ApiOperation("Lấy danh sách cuộc trò chuyện")
-    public ResponseEntity<?> getAllInboxOfCurrentUser(@ApiIgnore @AuthenticationPrincipal User user, Pageable pageable) {
+    public ResponseEntity<?> getAllInboxOfCurrentUser(@RequestParam Optional<RoomType> type, @ApiIgnore @AuthenticationPrincipal User user, Pageable pageable) {
         if (user == null)
             throw new UnAuthenticateException();
         Page<Inbox> inboxPage = inboxRepository.getAllInboxOfUser(user.getId(), pageable);
-        return ResponseEntity.ok(toInboxDto(inboxPage));
+        if (type.isPresent() && type.get().equals(RoomType.GROUP))
+            return ResponseEntity.ok(toInboxGroupDto(inboxPage));
+        else
+            return ResponseEntity.ok(toInboxDto(inboxPage));
     }
 
     /**
@@ -89,6 +110,82 @@ public class InboxRest {
         return ResponseEntity.badRequest().build();
     }
 
+    @GetMapping("/with/{anotherUserId}")
+    @PreAuthorize("isAuthenticated()")
+    @ApiOperation("Lấy inbox với người này nếu có")
+    public ResponseEntity<?> getAllInboxOfCurrentUser(@ApiIgnore @AuthenticationPrincipal User user, @PathVariable String anotherUserId) {
+        if (user == null)
+            throw new UnAuthenticateException();
+        if (anotherUserId == null)
+            return ResponseEntity.badRequest().build();
+        if (anotherUserId.equals(user.getId()))
+            return ResponseEntity.badRequest().build();
+        var room = roomRepository.findCommonRoomBetween(user.getId(), anotherUserId);
+        if (room == null) {
+            var r = new RoomOneSummaryDto();
+            r.setId(null);
+            r.setType(RoomType.ONE);
+            r.setTo(userMapper.toUserProfileDto(anotherUserId));
+            var dto = new InboxSummaryDto();
+            dto.setId(null);
+            dto.setRoom(r);
+            return ResponseEntity.ok(dto);
+        }
+        Optional<Inbox> inbox = inboxRepository.findByOfUserIdAndRoomId(user.getId(), room.getId());
+        if (inbox.isPresent())
+            return ResponseEntity.ok(inboxMapper.toInboxSummaryDto(inbox.get()));
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PostMapping("/with/{anotherUserId}")
+    @PreAuthorize("isAuthenticated()")
+    @ApiOperation("Tạo room và inbox mới cho anotherUserId")
+    public ResponseEntity<?> createNewRoomAndNewInbox(@ApiIgnore @AuthenticationPrincipal User user, @PathVariable String anotherUserId) {
+        if (user == null)
+            throw new UnAuthenticateException();
+        if (anotherUserId == null)
+            return ResponseEntity.badRequest().build();
+        if (anotherUserId.equals(user.getId()))
+            return ResponseEntity.badRequest().build();
+        var room = roomRepository.findCommonRoomBetween(user.getId(), anotherUserId);
+        if (room == null) {
+            Set<Member> members = new HashSet<>();
+            members.add(Member.builder().userId(user.getId()).build());
+            members.add(Member.builder().userId(anotherUserId).build());
+            var newRoom = Room.builder()
+                    .members(members)
+                    .type(RoomType.ONE)
+                    .build();
+            roomRepository.save(newRoom);
+            Inbox ib1 = Inbox.builder()
+                    .roomId(newRoom.getId())
+                    .ofUserId(user.getId())
+                    .build();
+            Inbox ib2 = Inbox.builder()
+                    .roomId(newRoom.getId())
+                    .ofUserId(anotherUserId)
+                    .build();
+            inboxRepository.save(ib1);
+            inboxRepository.save(ib2);
+            return ResponseEntity.ok(inboxMapper.toInboxSummaryDto(ib1));
+        } else {
+            Optional<Inbox> ib = inboxRepository.findByOfUserIdAndRoomId(user.getId(), room.getId());
+            if (ib.isPresent())
+                return ResponseEntity.ok(inboxMapper.toInboxSummaryDto(ib.get()));
+            Inbox ib1 = Inbox.builder()
+                    .roomId(room.getId())
+                    .ofUserId(user.getId())
+                    .build();
+            Inbox ib2 = Inbox.builder()
+                    .roomId(room.getId())
+                    .ofUserId(anotherUserId)
+                    .build();
+            inboxRepository.save(ib1);
+            inboxRepository.save(ib2);
+            return ResponseEntity.ok(inboxMapper.toInboxSummaryDto(ib1));
+        }
+    }
+
     /**
      * chuyển từ Page inbox sang page inboxDto
      */
@@ -97,6 +194,24 @@ public class InboxRest {
         List<InboxDto> dto = content.stream()
                 .map(x -> inboxMapper.toInboxDto(x))
                 .sorted()
+                .collect(Collectors.toList());
+        return new PageImpl<>(dto, inboxPage.getPageable(), inboxPage.getTotalElements());
+    }
+
+    private Page<?> toInboxGroupDto(Page<Inbox> inboxPage) {
+        List<Inbox> content = inboxPage.getContent();
+        List<InboxDto> dto = content.stream()
+                .map(x -> inboxMapper.toInboxDto(x))
+                .sorted()
+                .filter(x -> {
+                    try {
+                        RoomGroupSummaryDto r = (RoomGroupSummaryDto) x.getRoom();
+                        if (r.getType().equals(RoomType.GROUP))
+                            return true;
+                    } catch (Exception ignored) {
+                    }
+                    return false;
+                })
                 .collect(Collectors.toList());
         return new PageImpl<>(dto, inboxPage.getPageable(), inboxPage.getTotalElements());
     }
