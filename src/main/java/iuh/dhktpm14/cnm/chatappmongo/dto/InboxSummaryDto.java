5429fdc785bc7f45f18a992c73a5650b7bc0c41a
maikiencuong <maikiencuongiuh@gmail.com>
2021-09-19 15:34:20 +0700

    [dev] InboxSummaryDto, RoomOneSummaryDto: thêm thuộc tính null trong khi serializable ra json

--- src/main/java/iuh/dhktpm14/cnm/chatappmongo/dto/InboxSummaryDto.java
+++ src/main/java/iuh/dhktpm14/cnm/chatappmongo/dto/InboxSummaryDto.java
@@ -1,12 +1,11 @@
 package iuh.dhktpm14.cnm.chatappmongo.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
-@JsonInclude(JsonInclude.Include.NON_NULL)
+//@JsonInclude(JsonInclude.Include.NON_NULL)
 public class InboxSummaryDto implements Serializable {
     private String id;
     private Object room;
--- src/main/java/iuh/dhktpm14/cnm/chatappmongo/dto/RoomOneSummaryDto.java
+++ src/main/java/iuh/dhktpm14/cnm/chatappmongo/dto/RoomOneSummaryDto.java
@@ -1,14 +1,13 @@
 package iuh.dhktpm14.cnm.chatappmongo.dto;
 
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import iuh.dhktpm14.cnm.chatappmongo.enumvalue.RoomType;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
-@JsonInclude(JsonInclude.Include.NON_NULL)
+//@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RoomOneSummaryDto implements Serializable {
     private String id;
     private RoomType type;
