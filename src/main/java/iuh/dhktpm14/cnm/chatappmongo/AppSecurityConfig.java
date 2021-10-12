a3d3ce242fd8d3ce341f61fa8266968023e4c189
maikiencuong <maikiencuongiuh@gmail.com>
2021-10-12 19:37:28 +0700

    [dev] ignore security for swagger endpoint

--- src/main/java/iuh/dhktpm14/cnm/chatappmongo/AppSecurityConfig.java
+++ src/main/java/iuh/dhktpm14/cnm/chatappmongo/AppSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -82,6 +83,19 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
         http.headers().frameOptions().disable();
 
         http.addFilterBefore(appAuthenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    /*
+    ingnore security for swagger
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui/**",
+                "/webjars/**");
     }
 
 }
