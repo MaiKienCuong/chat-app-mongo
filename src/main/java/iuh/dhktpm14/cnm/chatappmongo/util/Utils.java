d86b47ac1569965eb8b869808e576931b1ed666f
maikiencuong <maikiencuongiuh@gmail.com>
2021-11-11 01:51:16 +0700

    [dev] thêm hàm bỏ dấu Tiếng Việt Utils.removeAccent() trong Utils - bỏ dấu Tiếng Việt trong tên của user để sắp xếp theo thứ tự a->z, nếu có dấu thì sắp xếp bị sai

--- src/main/java/iuh/dhktpm14/cnm/chatappmongo/util/Utils.java
+++ src/main/java/iuh/dhktpm14/cnm/chatappmongo/util/Utils.java
@@ -1,5 +1,20 @@
 package iuh.dhktpm14.cnm.chatappmongo.util;
 
-public class Utils {
-    public static final String REFRESH_TOKEN = "refresh_token";
+import java.text.Normalizer;
+import java.util.regex.Pattern;
+
+public interface Utils {
+    String REFRESH_TOKEN = "refresh_token";
+
+    /*
+    chuyển tiếng việt có dấu thành không dấu
+     */
+    static String removeAccent(String s) {
+        String temp = Normalizer.normalize(s, Normalizer.Form.NFD);
+        var pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+        return pattern.matcher(temp).replaceAll("")
+                .replace("đ", "d")
+                .replace("Đ", "D");
+    }
+
 }
